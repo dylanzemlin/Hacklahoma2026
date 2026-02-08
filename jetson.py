fcd2f9673018ee84732285300f420d88bd5565dd
Daniel Brown <is.a.squrril@gmail.com>
2026-02-08 02:57:38 -0600

    technically working and recognizing speech

--- jetson.py
+++ jetson.py
@@ -1,15 +1,28 @@
-#import whisper
+import whisper
 # fix alsalib errors?
 #import sounddevice
 
 import speech_recognition as sr
 import time
 
+model = whisper.load_model("small.en")
+
 def audio_callback(_, audio:sr.AudioData):
+	#global model
+
 	data = audio.get_raw_data()
-	print(data)
+
+	# send to file so we can send that to whisper
+	with open("tempaudiofile.wav", "wb") as f:
+		f.write(audio.get_wav_data())
+	#print(data)
+
+	print(model.transcribe("tempaudiofile.wav"))
 
 def main():
+	# big boi
+	#model = whisper.load_model("small.en")
+
 	recorder = sr.Recognizer()
 	recorder.energy_threshold = 3200 #TODO FIXME
 
@@ -28,6 +41,5 @@ def main():
 if __name__ == "__main__":
 	main()
 
-#model = whisper.load_model("small.en")
 #result = model.transcribe("audio.mp3")
 #print(result["text"])
