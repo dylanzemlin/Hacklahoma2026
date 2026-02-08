b1549a65099dfa931b8af58e9ce8f8e31389f9ce
Daniel Brown <is.a.squrril@gmail.com>
2026-02-08 03:34:44 -0600

    turbo too big

--- jetson.py
+++ jetson.py
@@ -1,45 +1,114 @@
-import whisper
-# fix alsalib errors?
-#import sounddevice
 
+# shamelessly stolen from https://github.com/davabase/whisper_real_time/blob/master/transcribe_demo.py
+
+import os
+import numpy as np
 import speech_recognition as sr
-import time
+import whisper
 
-model = whisper.load_model("small.en")
+from datetime import datetime, timedelta
+from queue import Queue
+from time import sleep
 
-def audio_callback(_, audio:sr.AudioData):
-	#global model
-
-	data = audio.get_raw_data()
-
-	# send to file so we can send that to whisper
-	with open("tempaudiofile.wav", "wb") as f:
-		f.write(audio.get_wav_data())
-	#print(data)
-
-	print(model.transcribe("tempaudiofile.wav"))
 
 def main():
-	# big boi
-	#model = whisper.load_model("small.en")
+    # The last time a recording was retrieved from the queue.
+    phrase_time = None
+    # Thread safe Queue for passing data from the threaded recording callback.
+    data_queue = Queue()
+    # Bytes object which holds audio data for the current phrase
+    phrase_bytes = bytes()
+    # We use SpeechRecognizer to record our audio because it has a nice feature where it can detect when speech ends.
+    recorder = sr.Recognizer()
+    recorder.energy_threshold = 3200
+    # Definitely do this, dynamic energy compensation lowers the energy threshold dramatically to a point where the SpeechRecognizer never stops recording.
+    recorder.dynamic_energy_threshold = False
 
-	recorder = sr.Recognizer()
-	recorder.energy_threshold = 3200 #TODO FIXME
+    # Important for linux users.
+    # Prevents permanent application hang and crash by using the wrong Microphone
+    source = sr.Microphone(device_index=0, sample_rate=16000)
 
-	source = sr.Microphone(device_index=0, sample_rate = 16000) #TODO FIXME
-	with source:
-		recorder.adjust_for_ambient_noise(source, duration = 3)
+    # Load / Download model
+    audio_model = whisper.load_model("small.en", download_root="/mnt/pepper/Hacklahoma2026/model")
 
-	recorder.listen_in_background(source, audio_callback, phrase_time_limit = 5)
+    # FIXME
+    record_timeout = 2
+    phrase_timeout = 3
 
-	while True:
-		try:
-			time.sleep(0.25)
-		except KeyboardInterrupt:
-			break
+    transcription = ['']
+
+    with source:
+        recorder.adjust_for_ambient_noise(source)
+
+    def record_callback(_, audio:sr.AudioData) -> None:
+        """
+        Threaded callback function to receive audio data when recordings finish.
+        audio: An AudioData containing the recorded bytes.
+        """
+        # Grab the raw bytes and push it into the thread safe queue.
+        data = audio.get_raw_data()
+        data_queue.put(data)
+
+    # Create a background thread that will pass us raw audio bytes.
+    # We could do this manually but SpeechRecognizer provides a nice helper.
+    recorder.listen_in_background(source, record_callback, phrase_time_limit=record_timeout)
+
+    # Cue the user that we're ready to go.
+    print("Model loaded.\n")
+
+    while True:
+        try:
+            now = datetime.utcnow()
+            # Pull raw recorded audio from the queue.
+            if not data_queue.empty():
+                phrase_complete = False
+                # If enough time has passed between recordings, consider the phrase complete.
+                # Clear the current working audio buffer to start over with the new data.
+                if phrase_time and now - phrase_time > timedelta(seconds=phrase_timeout):
+                    phrase_bytes = bytes()
+                    phrase_complete = True
+                # This is the last time we received new audio data from the queue.
+                phrase_time = now
+
+                # Combine audio data from queue
+                audio_data = b''.join(data_queue.queue)
+                data_queue.queue.clear()
+
+                # Add the new audio data to the accumulated data for this phrase
+                phrase_bytes += audio_data
+
+                # Convert in-ram buffer to something the model can use directly without needing a temp file.
+                # Convert data from 16 bit wide integers to floating point with a width of 32 bits.
+                # Clamp the audio stream frequency to a PCM wavelength compatible default of 32768hz max.
+                audio_np = np.frombuffer(phrase_bytes, dtype=np.int16).astype(np.float32) / 32768.0
+
+                # Read the transcription.
+                result = audio_model.transcribe(audio_np, fp16=False)
+                text = result['text'].strip()
+
+                # If we detected a pause between recordings, add a new item to our transcription.
+                # Otherwise edit the existing one.
+                if phrase_complete:
+                    transcription.append(text)
+                else:
+                    transcription[-1] = text
+
+                # Clear the console to reprint the updated transcription.
+                os.system('cls' if os.name=='nt' else 'clear')
+                for line in transcription:
+                    print(line)
+                # Flush stdout.
+                print('', end='', flush=True)
+            else:
+                # Infinite loops are bad for processors, must sleep.
+                sleep(0.25)
+        except KeyboardInterrupt:
+            break
+
+    print("\n\nTranscription:")
+    for line in transcription:
+        print(line)
+
 
 if __name__ == "__main__":
-	main()
-
-#result = model.transcribe("audio.mp3")
-#print(result["text"])
+    main()
